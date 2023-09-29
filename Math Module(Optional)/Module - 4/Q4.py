cccf571bc6e4000852b184544a1f967592334f21
redoy1102 <hridoyvlog9@gmail.com>
2023-09-29 19:29:41 +0600

    Problem of Ternary numbers are formed with a number system with base 3. Given the

---
+++ Math Module(Optional)/Module - 4/Q4.py
@@ -0,0 +1,13 @@
+# Ternary numbers are formed with a number system with base 3. Given the
+# ternary number 10212, find its decimal value. Write in Details.
+
+num = int(input())
+s = 0
+i = 0
+while num != 0:
+    lastDigit = num % 10
+    s = s + (lastDigit * pow(3, i))
+    num = num // 10
+    i += 1
+
+print(s)
