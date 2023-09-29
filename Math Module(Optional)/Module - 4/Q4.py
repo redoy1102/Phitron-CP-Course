09125a5960bfe46cb12f6334688d7c33dbc90bae
redoy1102 <hridoyvlog9@gmail.com>
2023-09-29 19:33:55 +0600

    Problem 4 solved

--- Math Module(Optional)/Module - 4/Q4.py
+++ Math Module(Optional)/Module - 4/Q4.py
@@ -2,12 +2,12 @@
 # ternary number 10212, find its decimal value. Write in Details.
 
 num = int(input())
-s = 0
+total = 0
 i = 0
 while num != 0:
     lastDigit = num % 10
-    s = s + (lastDigit * pow(3, i))
+    total = total + (lastDigit * pow(3, i))
     num = num // 10
     i += 1
 
-print(s)
+print(total)
