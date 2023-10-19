738e5db656aacd882f2207b1dd922d8e71590e38
redoy1102 <hridoyvlog9@gmail.com>
2023-10-19 22:48:19 +0600

    A. Create A New String accepted

---
+++ Week - 03/Module - 10/Practice problems/A_Create_A_New_String.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<limits.h>
+#include<string.h>
+
+int main(){
+    char s[1001], t[1001];
+    scanf("%s%s", s, t);
+    // scanf("%s", t);
+
+    printf("%d %d\n", strlen(s), strlen(t));
+
+    printf("%s %s\n", s, t);
+
+    return 0;
+}
