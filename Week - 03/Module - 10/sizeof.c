c6c4edbd5cdd64f71d1129fd28f5741423a49fa1
redoy1102 <hridoyvlog9@gmail.com>
2023-10-22 22:26:06 +0600

    sizeof() is added in module 10

---
+++ Week - 03/Module - 10/sizeof.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    char name[5];
+    int size = sizeof(name);
+    printf("%d\n", size);
+
+    return 0;
+}
---
+++ Week - 03/Module - 11/stringCopy.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    // char ch;
+    // scanf("%c", &ch);
+    char ch[5] = "Fuad";
+    
+
+
+
+    return 0;
+}
