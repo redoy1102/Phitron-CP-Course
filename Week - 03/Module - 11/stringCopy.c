6a83c3823b901aad2d97c9b221da759fb300b27a
redoy1102 <hridoyvlog9@gmail.com>
2023-10-22 22:36:50 +0600

    String copy done

--- Week - 03/Module - 11/stringCopy.c
+++ Week - 03/Module - 11/stringCopy.c
@@ -1,10 +1,16 @@
 #include<stdio.h>
 #include<string.h>
 int main(){
-    // char ch;
-    // scanf("%c", &ch);
-    char ch[5] = "Fuad";
-    
+    char a[5], b[5];
+    printf("Enter two string: ");
+    scanf("%s %s", a, b);
+    // printf("%s %s", a, b);
+
+    // for(int i = 0; i <= strlen(b); i++){
+    //     a[i] = b[i];
+    // }
+    strcpy(a, b);
+    printf("%s %s", a, b);
 
 
 
