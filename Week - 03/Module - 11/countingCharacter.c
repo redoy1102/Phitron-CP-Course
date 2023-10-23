ae23df84ff2851279916e8cb86453bb88080373d
redoy1102 <hridoyvlog9@gmail.com>
2023-10-23 21:37:40 +0600

    Different way of showing output of counting array is added

--- Week - 03/Module - 11/countingCharacter.c
+++ Week - 03/Module - 11/countingCharacter.c
@@ -9,9 +9,30 @@ int main(){
         int value = ch[i]-97;
         ca[value]++;
     }
-    for(int i = 0; i < 26; i++){
-        printf("%c - %d\n", i+97, ca[i]);
+
+    // way of output - 1
+    // for(int i = 0; i < 26; i++){
+    //     if(ca[i] != 0){
+    //         printf("%c - %d\n", i+97, ca[i]);
+    //     }
+    // }
+
+    // way of output - 2
+    for(int i = 0; i < strlen(ch); i++){
+        int value = ch[i] - 97;
+        
+        printf("%c - %d\n",value+97, ca[value]);
     }
+    
+    // way of output - 3
+    // for(int i = 0; i < strlen(ch); i++){
+    //     int value = ch[i] - 97;
+        
+    //     if(ca[value] != 0){
+    //         printf("%c - %d\n",value+97, ca[value]);
+    //     }
+    //     ca[value] = 0;
+    // }
 
 
 
