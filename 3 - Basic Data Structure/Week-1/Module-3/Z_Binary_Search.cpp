9190ae5db73bc4a5c0c9b87ee41d84116905f4db
redoy1102 <hridoyvlog9@gmail.com>
2024-04-23 13:01:44 +0600

    binary search recap

--- 3 - Basic Data Structure/Week-1/Module-3/Z_Binary_Search.cpp
+++ 3 - Basic Data Structure/Week-1/Module-3/Z_Binary_Search.cpp
@@ -4,36 +4,35 @@ void solve(){
     int n, q;
     cin>>n>>q;
     vector<int> v(n);
-    for (int i = 0; i < n; i++)
+    for(int i =0; i < n; i++)
     {
-        cin >> v[i];
+        cin>>v[i];
     }
     sort(v.begin(), v.end());
 
-    while (q--)
+    while(q--)
     {
-        int x;
-        cin>>x;
-        int l = 0, r = n - 1;
-        bool flag = false;
+        int x;cin>>x;
+        int l=0, r=n-1;
+        bool b = false;
         while(l <= r)
         {
-            int mid = (l + r) / 2;
-            if(v[mid] == x){
-                flag=true;
+            
+            int mid=(l+r) / 2;
+            if(v[mid] == x)
+            {
+                b=true;
                 break;
             }
             else if(x > v[mid]){
-                l = mid + 1;
+                l=mid+1;
             }
             else{
-                r = mid - 1;
+                r=mid-1;
             }
         }
-        if(flag)
-            cout << "found" << endl;
-        else
-            cout << "not found" << endl;
+        if(b)   cout<<"found"<<endl;
+        else cout<<"not found"<<endl;
     }
 }
 int main(){
