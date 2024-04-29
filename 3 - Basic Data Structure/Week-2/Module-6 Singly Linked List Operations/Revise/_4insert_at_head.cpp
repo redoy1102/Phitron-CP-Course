e1b9151a120243a06642859d30b4e6f56a6c7ee3
redoy1102 <hridoyvlog9@gmail.com>
2024-04-29 17:01:22 +0600

    Prayer brake

---
+++ 3 - Basic Data Structure/Week-2/Module-6 Singly Linked List Operations/Revise/_4insert_at_head.cpp
@@ -0,0 +1,111 @@
+#include<bits/stdc++.h>
+#define endl '\n'
+#define long long int int
+using namespace std;
+class Node{
+    public:
+    int value;
+    Node *next;
+    Node(int value)
+    {
+        this->value=value;
+        this->next=NULL;
+    }
+};
+
+void insert_at_tail(Node *&head)
+{
+    int v;  cin>>v;
+    Node *newNode = new Node(v);
+
+    if(head==NULL)
+    {
+        head = newNode;
+        return;
+    }
+    Node *temp=head;
+    while(temp->next != NULL)
+    {
+        temp=temp->next;
+    }
+    temp->next=newNode;
+    cout<<v<<" is added in tail"<<endl;
+}
+void print(Node *head)
+{
+    cout<<"Values of the list: ";
+    Node *temp = head;
+    while(temp != NULL)
+    {
+        cout<<temp->value<<" ";
+        temp=temp->next;
+    }
+    cout<<endl;
+}
+void insert_at_head(Node *&head, int v)
+{
+    Node *newNode = new Node(v);
+    newNode->next=head;
+    head=newNode;
+}
+void insert_at_any(Node *&head)
+{
+    cout<<"Enter the value: ";
+    int v;  cin>>v;
+    Node *newNode = new Node(v);
+
+    cout<<"Enter the index number: ";
+    int index;  cin>>index;
+
+    if(index == 0)
+    {
+        insert_at_head(head, v);
+        return;
+    }
+
+    Node *temp=head;
+    index--;
+    while(index--)
+    {
+        temp=temp->next;
+    }
+    newNode->next=temp->next;
+    temp->next=newNode;
+    cout<<v<<" is added."<<endl;
+}
+
+void solve()
+{
+    Node *head = NULL;
+    while(true)
+    {
+        cout<<"1 for print."<<endl;
+        cout<<"2 for insert at tail"<<endl;
+        cout<<"3 for insert at any"<<endl;
+        cout<<"4 for insert at head."<<endl;
+        cout<<"0 for break"<<endl;
+        cout<<"Choose your option: ";
+        int opt;    cin>>opt;
+        if(opt==1)  print(head);
+        else if(opt==2) insert_at_tail(head);
+        else if(opt==3) insert_at_any(head);
+        else if(opt==4){
+            int value; cin>>value;
+            insert_at_head(head, value);
+        }
+        
+        else    break;
+    }
+}
+int main()
+{
+    int t;
+    // cin >> t;
+    t = 1;
+    while(t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
